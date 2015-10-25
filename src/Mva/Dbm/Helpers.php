c8c9478b2ccaa578157cec23bd57750e335a45bd
Roman Vykuka <vykuka@gmail.com>
2015-10-25 21:01:02 +0100

    Created new class Helpers, fetchPairs functionality moved to the Helpers

---
+++ src/Mva/Dbm/Helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mva\Dbm;
+
+class Helpers
+{
+
+	public static function fetchPairs($data, $key = NULL, $value = NULL)
+	{
+		$return = [];
+
+		if (!is_array($data) && !$data instanceof \Traversable) {
+			throw new InvalidArgumentException('Parameter $data must be array or Traversable object!');
+		}
+
+		if ($key === NULL && $value === NULL) {
+			throw new InvalidArgumentException('FetchPairsHelper requires defined key or value!');
+		}
+
+		if ($key === NULL) {
+			foreach ($data as $row) {
+				$return[] = $row[$value];
+			}
+		} elseif ($value === NULL) {
+			foreach ($data as $row) {
+				$return[is_object($row[$key]) ? (string) $row[$key] : $row[$key]] = $row;
+			}
+		} else {
+			foreach ($data as $row) {
+				$return[is_object($row[$key]) ? (string) $row[$key] : $row[$key]] = $row[$value];
+			}
+		}
+
+		return $return;
+	}
+
+}
